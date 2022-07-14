04eeba1f4f7316aa0a9debc6d3b686722ec4a6a4
Eduardo Gonçalves <eduardo.alexandretti@appmax.com.br>
2022-07-13 22:10:35 -0300

    Criado migration, model, controller

---
+++ src/database/migrations/2022_07_14_000810_create_compras_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao');
+            $table->integer('categoria_id');
+            $table->decimal('valor');
+            $table->integer('cartao_id');
+            $table->date('data');
+            $table->string('usuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compras');
+    }
+};
