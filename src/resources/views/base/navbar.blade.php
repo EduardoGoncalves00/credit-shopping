88e1235ca418c39a6f6f4416b923b5a7b5ab73aa
EduardoGoncalves00 <dudugoncalves20@gmail.com>
2022-08-29 22:50:44 -0300

    Ajustes no componente bootstrap

---
+++ src/resources/views/base/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                COMPRAS
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="{{ route('lista') }}">Lista de compras</a>
+                <a class="dropdown-item" href="{{ route('compras') }}">Criar compra</a>
+                <a class="dropdown-item" href="{{ route('relatorios') }}">Relatório</a>
+            </div>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    CARTÕES
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="{{ route('cartoes') }}">Lista de cartões</a>
+                    <a class="dropdown-item" href="{{ route('criar_cartoes') }}">Criar cartão</a>
+                </div>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    CATEGORIAS
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="{{ route('categorias') }}">Lista de categorias</a>
+                </div>
+            </li>
+            </li>
+        </ul>
+    </div>
+  </nav>
--- src/resources/views/cartoes/criar.blade.php
+++ src/resources/views/cartoes/criar.blade.php
@@ -1,26 +1,57 @@
-<a href='{{ route('cartoes') }}'>clique aqui para listar todos cartoes</a>
+@extends('base.layout')
+@section('titulo', "Criar cartão")
 
-<a href='{{ route('lista') }}'>clique aqui para ir a lista de compras</a>
+@section('conteudo')
+<br>
+<div class="card container w-25">
+    <h4 class="card-title text-center">Criar cartão</h4>
 
+    <form action="{{route('criar_cartao')}}" method="POST">
+        @csrf
+        <div class="form-group @error('nome') is-invalid @enderror">
+            <label for="nome" >Nome do cartão</label>
+            <input type="text" class="form-control" name="nome" placeholder="Ex: Itau - Eduardo">
+            @error('nome')
+            <div class="invalid-feedaback text-danger">
+                {{$message}}
+            </div>
+            @enderror
+        </div>
+        <br>
+        <div class="form-group @error('dia_pagamento') is-invalid @enderror">
+            <label for="dia_pagamento" >Dia de pagamento</label>
+            <input type="number" class="form-control" name="dia_pagamento" placeholder="Ex: 10">
+            @error('dia_pagamento')
+            <div class="invalid-feedaback text-danger">
+                {{$message}}
+            </div>
+            @enderror
+        </div>
+        <br>
+        <div class="form-group @error('dia_fechamento') is-invalid @enderror">
+            <label for="dia_fechamento" >Dia de fechamento</label>
+            <input type="number" class="form-control" name="dia_fechamento" placeholder="Ex: 03">
+            @error('dia_fechamento')
+            <div class="invalid-feedaback text-danger">
+                {{$message}}
+            </div>
+            @enderror
+        </div>
+        <br>
+        <div class="form-group @error('banco') is-invalid @enderror">
+            <label for="banco" >Banco</label>
+            <input type="text" class="form-control" name="banco" placeholder="Ex: Itau">
+            @error('banco')
+            <div class="invalid-feedaback text-danger">
+                {{$message}}
+            </div>
+            @enderror
+        </div>
+        <br>
 
-<form action="{{route('criar_cartao')}}" method="POST">
-    <br>
-    @csrf
-    <div class="form-group">
-      <input type="text" class="form-control" name="nome" placeholder="Nome do cartão">
-    </div>
-    <br>
-    <div class="form-group">
-        <input type="number" class="form-control" name="dia_pagamento" placeholder="Dia de pagamento">
-    </div>
-    <br>
-    <div class="form-group">
-        <input type="number" class="form-control" name="dia_fechamento" placeholder="Dia de fechamento">
-    </div>
-    <br>
-    <div class="form-group">
-        <input type="text" class="form-control" name="banco" placeholder="Banco">
-    </div>
-    <br>
-    <button type="submit" class="btn btn-primary">Salvar</button>
-</form>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary">Salvar</button>
+        </div>
+    </form>
+</div>
+@endsection
